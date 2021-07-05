3403736ebd8b64d7da832f3fd6eb66fd24ab9535
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-05 09:44:34 +0700

    4. Components & Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import CharacterCard from './CharacterCard';
 
+const word = "Hello";
 function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
+ return (
+ <div className="card">
+ {
+ Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ }
+ </div>
+ );
 }
-
 export default App;
