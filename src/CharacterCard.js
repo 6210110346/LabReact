5a0ec05a6207aa46bf5f9c2864e56a53e22b93ea
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2021-07-05 10:16:07 +0700

    5. Components & States

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 
 export default function CharacterCard(props) {
-    const bool = false
-    const [active, setActive] = useState(bool);
-    const activate = () =>{setActive(!bool)}
-
-    const className = `card ${active ? 'activeCard': ''}`
+    const attemptRef = useRef(props.attempt);
+    const [active, setActive] = useState(false);
+    const activate = () => {
+        setActive(true)
+        props.activationHandler(props.value)
+    }
+    useEffect(() => {
+        if(attemptRef.current != props.attempt){
+        setActive(false)
+        attemptRef.current = props.attempt
+        }
+        })
+    const className = `card ${active ? 'activeCard' : ''}`
     return (
         <div className={className} onClick={activate}>{props.value}</div>
     )
